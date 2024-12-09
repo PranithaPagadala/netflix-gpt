51401760fd5f8cf12389a0e69d3b2817fe048767
PranithaPagadala <pagadalapranitha456@gmail.com>
2024-12-09 12:03:11 +0530

    cra&tailwind

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+        <Header/>
+        <div>
+            <img alt='img' src='https://assets.nflxext.com/ffe/siteui/vlv3/ce449112-3294-449a-b8d3-c4e1fdd7cff5/web/IN-en-20241202-TRIFECTA-perspective_0acfb303-6291-4ad1-806f-dda785f6295a_large.jpg'/>
+        </div>
+    </div>
+  )
+}
+
+export default Login
